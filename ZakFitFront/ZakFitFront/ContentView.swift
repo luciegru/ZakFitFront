459cec6d0f86ec9f93d5eb6f3c68cad6e547b15c
Lucie Grunenberger <lucie.grunenberger@gmail.com>
2025-11-23 17:41:48 +0100

    Initial project setup with MVVM architecture

---
+++ ZakFitFront/ZakFitFront/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZakFitFront
+//
+//  Created by Lucie Grunenberger  on 23/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
